09b0fafacad49cf2641013ca6f49519eedb41bae
Koami Azanleko <haya@flexix.nl>
2026-04-27 14:00:08 +0200

    DATABASE_URL connexion Supabase

--- database.js
+++ database.js
@@ -2,17 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: 'postgres',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
 pool.connect((err) => {
   if (err) {
-    console.error('❌ Erreur connexion base de données:', err.message);
+    console.error('❌ Erreur connexion base de données:', err.message || err);
   } else {
     console.log('✅ Connecté à la base de données Supabase !');
   }
